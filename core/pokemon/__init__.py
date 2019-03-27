1ab00a67f6a9c264d624797f771247c94c021187
Luke Brady <lbrad23105@gmail.com>
2019-03-27 15:53:03 -0400

    Can retrieve pokemon information from the API.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

---
+++ core/pokemon/__init__.py
@@ -0,0 +1,31 @@
+# This file contains functions that reads a random Pokemon to disk and returns the JSON object with its details.
+import requests
+import random
+
+# read_poke_file reads the list of Pokemon and returns the list.
+def read_poke_file():
+    # The array that will be returned with the list of Pokemon.
+    pokemon = []
+    with open('Configuration/pokemon_list.txt') as p:
+        for line in p:
+            pokemon.append(line[:-1])
+    return pokemon
+
+# get_random_pokemon chooses a random Pokemon.
+def get_random_pokemon(pokemon_list):
+    num = random.randint(0, len(pokemon_list) - 1)
+    return pokemon_list[num]
+
+# get_pokemon_info retrieves a pokemon's statistics from the pokemon API.
+def get_pokemon_info(pokemon):
+
+    # The URL that will be queried.
+    url = 'https://pokeapi.co/api/v2/pokemon/{}'.format(str(pokemon).lower())
+    print(url)
+    # GET the pokemons information.
+    try:
+        req =requests.get(url=url)
+        return req.json()
+    except requests.HTTPError as e:
+        print(e)
+    return None
--- pokemail.py
+++ pokemail.py
@@ -1,5 +1,10 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 
-from core import smtp as ps
+from core import pokemon
 
-ps.send_pokemon('h',[])
+#ps.send_pokemon('h',[])
+poke_arr = pokemon.read_poke_file()
+p = pokemon.get_random_pokemon(pokemon_list=poke_arr)
+info = pokemon.get_pokemon_info(p)
+
+print(info['name'])
