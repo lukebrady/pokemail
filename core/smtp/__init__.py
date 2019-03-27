46d7c8f51182b218b40fd9523b7cb680818a9b5e
Luke Brady <lbrad23105@gmail.com>
2019-03-27 16:19:04 -0400

    Can send email with Pokemon attached.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- core/smtp/__init__.py
+++ core/smtp/__init__.py
@@ -1,6 +1,52 @@
-import smtplib, ssl
+import json
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+# read_mail_config reads user email and password from config.json.
+def read_mail_config():
+    with open('core/smtp/config.json') as conf:
+        c = conf.read()
+    return json.loads(c)
 
 
 # send_pokemon sends a random pokemon to the list of subscribers.
-def send_pokemon(pokemon, users):
+def send_pokemon(pokemon, image, users):
     # Create the SMTP client that will send pokemail.
+    conf_obj = read_mail_config()
+
+    sender_email = conf_obj['email']
+    receiver_email = "luke.brady@ung.edu"
+    password = conf_obj['password']
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "multipart test"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    html = """\
+    <html>
+      <body>
+        <h3>A wild {}{} has appeared.</h3>
+        <img src = "{}"/>
+      </body>
+    </html>
+    """.format(str(pokemon[0]).upper(), pokemon[1:], image)
+
+    # Turn these into plain/html MIMEText objects
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part2)
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
--- pokemail.py
+++ pokemail.py
@@ -1,10 +1,12 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 
 from core import pokemon
+from core import smtp
 
-#ps.send_pokemon('h',[])
 poke_arr = pokemon.read_poke_file()
 p = pokemon.get_random_pokemon(pokemon_list=poke_arr)
 info = pokemon.get_pokemon_info(p)
+smtp.send_pokemon(info['name'],info['sprites']['front_default'],[])
+print(info['sprites']['front_default'])
 
 print(info['name'])
